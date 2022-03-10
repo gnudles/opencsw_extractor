4a7ff204bb6995d7f4fb9754cd5b6c338353b1be
Kapandaria <dvoreader@gmail.com>
2022-03-11 00:25:40 +0200

    Create pkg_extract.sh

---
+++ pkg_extract.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# We ungzip the gz file
+# We skip the first 512 bytes of package datastream header, and then we get a normal svr4 archive for cpio. almost normal.
+# We replace the early terminator of "TRAILER!!!", in order to bypass this strange hack. otherwise, we will only get the first two files.
+# We hope that the pattern we match here, is very unique, otherwise we screw things up inside file content. The alternative is to create cpio fork, that ignores that strange TRAILER.
+# We let cpio do the rest...
+gzip -cdk $1 | tail -c +513 |sed  's/0000TRAILER!!!\x0\x0\x0\x0\x0\x0\x0/0000SPOILER!!!\x0\x0\x0\x0\x0\x0\x0/g' | cpio -idu -0 -v -H newc --no-absolute-filenames
